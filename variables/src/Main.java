1f783396c2fdac29a265690bdf38d0cef5507a2f
mustafa <mustafaciceknote8@gmail.com>
2023-04-05 15:21:25 +0300

    first commit

---
+++ variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOfStudent = 10;
+        String message = "number of student : ";
+        System.out.println( message + numberOfStudent);
+        System.out.println( message + numberOfStudent);
+        System.out.println( message + numberOfStudent);
+        System.out.println( message + numberOfStudent);
+    }
+}
