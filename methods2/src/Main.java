a02c1a09277187fad467c5eab67b9ef0ff987ed4
mustafa <mustafaciceknote8@gmail.com>
2023-04-06 03:00:54 +0300

    commit

--- methods2/src/Main.java
+++ methods2/src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String message = "Bugün hava çok güzel.";
+        String newMessage = sehirVer();
+        System.out.println(newMessage);
+        int number = topla(81,12);
+        System.out.println(number);
+        int toplam = topla2(1,2,3,523);
+        System.out.println(toplam);
+
+    }
+
+    public static void add() {
+        System.out.println("eklendi");
+    }
+
+    public static void delete() {
+        System.out.println("silindi");
+
+    }
+
+    public static void update() {
+        System.out.println("güncellendi");
+    }
+
+    public static int topla(int number1,int number2) {
+
+        return number1+number2;
+    }
+    public static int topla2(int... sayilar){
+        int toplam =0;
+        for (int sayi: sayilar){
+            toplam+=sayi;
+        }
+        return toplam;
+    }
+
+    public static String sehirVer() {
+        return "ankara";
     }
 }
